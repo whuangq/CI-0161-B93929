918c4d3d3581865ab49a5525de45da4b4d33577f
Wilson Huang <dragonwilsonx60@gmail.com>
2023-11-26 14:29:55 -0600

    video continuacion 1

--- firebase/firebase_app/lib/presentation/screens/home_screen.dart
+++ firebase/firebase_app/lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_app/presentation/blocs/auth_cubit/auth_cubit.dart';
+import 'package:firebase_app/presentation/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -10,10 +11,10 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final authCubit = context.watch<AuthCubit>();
-
+    final colors = Theme.of(context).colorScheme;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Pagina de Inicio"),
+        title: const Text("Pizarra de mensajes"),
         actions: [
           IconButton(
             onPressed: () {
@@ -25,7 +26,46 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text(authCubit.state.email),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              const SizedBox(height: 25,),
+              const Expanded(child: SizedBox()),
+              const SizedBox(height: 25,),
+              Row(
+                children: [
+                  const Expanded(
+                    child: CustomTextField(
+                      hintText: "Escribe un mensaje",
+                    ),
+                  ),
+                  const SizedBox(width: 15,),
+                  IconButton.filled(
+                    onPressed: (){},
+                    icon: const Icon(Icons.send_rounded)
+                  )
+                ],
+              ),
+              const SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Registrado como: ',
+                    style: TextStyle(color: colors.secondary),
+                  ),
+                  const SizedBox(width: 5,),
+                  Text(
+                    authCubit.state.email,
+                    style: TextStyle(color: colors.primary, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15,),
+            ],
+          ),
+        )
       ),
     );
   }
